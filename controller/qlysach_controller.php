7610dc4c0f8c7fcae16b58971c7bc7f2933b5bdf
Yuno2204 <dqduc2204@gmail.com>
2025-01-10 15:49:24 +0700

    done

--- controller/qlysach_controller.php
+++ controller/qlysach_controller.php
@@ -64,30 +64,38 @@ switch ($request_method) {
         }
         break;
 
-    case 'POST':
-        $data = json_decode(file_get_contents("php://input"), true);
-        if (!isset($data['sach_id'], $data['tacgia_id'], $data['theloai_id'], $data['nxb_id'], $data['mota_sach'], $data['ten_sach'], $data['soluong_tonkho'])) {
-            http_response_code(422);
-            echo json_encode(["message" => "Thiếu dữ liệu đầu vào!"]);
+        case 'POST':
+            $data = json_decode(file_get_contents("php://input"), true);
+            if (!isset($data['sach_id'], $data['tacgia_id'], $data['theloai_id'], $data['nxb_id'], $data['mota_sach'], $data['ten_sach'], $data['soluong_tonkho'])) {
+                http_response_code(422);
+                echo json_encode(["message" => "Thiếu dữ liệu đầu vào!"]);
+                break;
+            }
+        
+            $sachId = intval($data['sach_id']);
+            $tacgiaId = intval($data['tacgia_id']);
+            $theloaiId = intval($data['theloai_id']);
+            $nxbId = intval($data['nxb_id']);
+            $mota = $data['mota_sach'];
+            $tenSach = $data['ten_sach'];
+            $soluong = intval($data['soluong_tonkho']);
+        
+            // Kiểm tra nếu số lượng tồn kho là số âm
+            if ($soluong < 0) {
+                http_response_code(400); // Lỗi người dùng gửi sai yêu cầu
+                echo json_encode(["message" => "Số lượng tồn kho không thể là số âm!"]);
+                break;
+            }
+        
+            if ($sachModel->addSach($sachId, $tacgiaId, $theloaiId, $nxbId, $mota, $tenSach, $soluong)) {
+                http_response_code(201);
+                echo json_encode(["message" => "Thêm sách thành công!"]);
+            } else {
+                http_response_code(500);
+                echo json_encode(["message" => "Thêm sách thất bại!"]);
+            }
             break;
-        }
-
-        $sachId = intval($data['sach_id']);
-        $tacgiaId = intval($data['tacgia_id']);
-        $theloaiId = intval($data['theloai_id']);
-        $nxbId = intval($data['nxb_id']);
-        $mota = $data['mota_sach'];
-        $tenSach = $data['ten_sach'];
-        $soluong = intval($data['soluong_tonkho']);
-
-        if ($sachModel->addSach($sachId, $tacgiaId, $theloaiId, $nxbId, $mota, $tenSach, $soluong)) {
-            http_response_code(201);
-            echo json_encode(["message" => "Thêm sách thành công!"]);
-        } else {
-            http_response_code(500);
-            echo json_encode(["message" => "Thêm sách thất bại!"]);
-        }
-        break;
+        
 
     case 'PUT':
         $data = json_decode(file_get_contents("php://input"), true);
